b7766133355230440bfe86a4e1a962cf7efa46b0
svincent <svincent@tbkcreative.com>
2019-06-04 11:28:48 -0400

    Update Story

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,12 +1,20 @@
 import { document, console } from 'global';
 import { storiesOf } from '@storybook/html';
+import { text, boolean, number } from '@storybook/addon-knobs';
 
-storiesOf('Demo', module)
-  .add('heading', () => '<h1>Hello World</h1>')
+const stories = storiesOf('Storybook Knobs', module);
+
+stories.addDecorator(withKnobs)
+  .add('heading', () => {
+    const hello = '<h1>hellow</h1>';
+    return hello;
+  })
   .add('button', () => {
     const button = document.createElement('button');
     button.type = 'button';
     button.innerText = 'Hello Button';
+    button.className = text('class', 'btn-danger');
+    button.disabled = boolean('disabled', false);
     button.addEventListener('click', e => console.log(e));
     return button;
   });
